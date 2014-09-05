1f7c5a148087059045781425ae555945aaa70df4
Anthony Ferrara <ircmaxell@gmail.com>
2014-09-04 21:01:22 -0400

    Add proper prefix, travis really needs some way to test locally

--- travis/build-jitfu.sh
+++ travis/build-jitfu.sh
@@ -2,7 +2,7 @@
 set -e
 cd $TRAVIS_BUILD_DIR/build/ext
 phpize
-./configure
+./configure --with-jitfu=$HOME
 make -j2 --quiet
 make install
 echo "extension=jitfu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
